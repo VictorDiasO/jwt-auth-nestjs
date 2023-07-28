dafbd6f03ed5abf0b4ecf61240110197c845def7
VictorDiasO <victordiasdeveloper@gmail.com>
2023-07-27 23:42:23 -0300

    Setting up the Validation Pipe

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class AuthDto {
+  @IsNotEmpty({ message: "It can't be empty" })
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({ message: "It can't be empty" })
+  @IsString()
   password: string;
 }
