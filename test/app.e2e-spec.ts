cbba0b5d6997b2a2131628fc542aec5015e33bcc
VictorDiasO <victordiasdeveloper@gmail.com>
2023-07-31 20:30:58 -0300

    Starting the tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AppModule } from 'src/app.module';
+import { AuthDto } from 'src/auth/dto';
+import { Tokens } from 'src/auth/types';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
+
+    prisma = app.get<PrismaService>(PrismaService);
+    await prisma.cleanDatabase();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@test.com',
+      password: 'password',
+    };
+
+    let tokens: Tokens;
+
+    it('Should Signup', () => {
+      return request(app.getHttpServer())
+        .post('/auth/local/signup')
+        .send(dto)
+        .expect(201)
+        .expect(({ body }: { body: Tokens }) => {
+          expect(body.access_token).toBeTruthy();
+          expect(body.refresh_token).toBeTruthy();
+        });
+    });
   });
 });
